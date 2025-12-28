8ad93c284239ee2a790e3d34a2d5285ff6464719
Michel Perez <michel.perez@gmx.ch>
2025-12-28 18:16:34 +0100

    add slugify function

--- diffusion/utils.py
+++ diffusion/utils.py
@@ -1,3 +1,5 @@
+import re
+import unicodedata
 from abc import ABC, abstractmethod
 
 import torch
@@ -505,3 +507,24 @@ class EulerSampler:
         noise_pred = self.model(x_t, timesteps[-1], context)
         # ...and return the final prediction.
         return self.step(x_t, noise_pred, gamma_t[-1])
+
+
+def slugify(value, allow_unicode=False):
+    """
+    Taken from https://github.com/django/django/blob/master/django/utils/text.py
+    Convert to ASCII if 'allow_unicode' is False. Convert spaces or repeated
+    dashes to single dashes. Remove characters that aren't alphanumerics,
+    underscores, or hyphens. Convert to lowercase. Also strip leading and
+    trailing whitespace, dashes, and underscores.
+    """
+    value = str(value)
+    if allow_unicode:
+        value = unicodedata.normalize("NFKC", value)
+    else:
+        value = (
+            unicodedata.normalize("NFKD", value)
+            .encode("ascii", "ignore")
+            .decode("ascii")
+        )
+    value = re.sub(r"[^\w\s-]", "", value.lower())
+    return re.sub(r"[-\s]+", "-", value).strip("-_")
