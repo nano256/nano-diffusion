b797f498d50f6d9682db211ed272c63beba855a0
Michel Perez <michel.perez@gmx.ch>
2026-02-06 11:58:38 +0100

    add noise scheduling notebook

--- notebooks/utils.py
+++ notebooks/utils.py
@@ -1,7 +1,12 @@
+from io import BytesIO
 from pathlib import Path
 
 import ipywidgets as widgets
 import mlflow
+import requests
+import torch
+from PIL import Image
+from torchvision.transforms.functional import to_pil_image, to_tensor
 
 
 def create_mlflow_run_selector(mlruns_path=None):
@@ -54,3 +59,27 @@ def create_mlflow_run_selector(mlruns_path=None):
         update_runs({"new": exp_selector.value})
 
     return exp_selector, run_selector
+
+
+def convert_image_to_normalized_tensor(img):
+    t = to_tensor(img)
+    mean = t.mean()
+    std = t.std()
+    return (t - mean) / std, mean, std
+
+
+def convert_normalized_tensors_to_images(t, mean, std):
+    img = torch.clip((t * std + mean), 0, 1)
+
+    if len(img.shape) <= 3:
+        return [to_pil_image(img)]
+
+    img_list = []
+    for i in range(img.shape[0]):
+        img_list.append(to_pil_image(img[i]))
+    return img_list
+
+
+def get_picsum_image(id, resolution=256):
+    image_url = f"https://picsum.photos/id/{id}/{resolution}/{resolution}"
+    return Image.open(BytesIO(requests.get(image_url).content))
